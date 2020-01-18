ab985a78ee56b3f89504ca2bdd52ceba30ebd1cb
faz1l <gl1t428@gmail.com>
2020-01-18 20:13:07 +0300

    Added the ability to choose the random generator params.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ use rand::Rng; // Package to create a random number
 use colored::*; // Package to colorize the output
 
 fn main() {
-    println!("{}\n", "***___NUM GUESSS GAME___***".yellow());
-    println!("{}\n\n", "Guess the number: ".blue());
+    println!("{}\n\n", "***___NUM GUESSS GAME___***".yellow()); // Print the title!
 
-    let secret_number = rand::thread_rng().gen_range(0, 5); // Generate the random number
+    println!("{}", "Enter the first random number param: ".blue()); // Ask the user to input the first param
+    let mut first_param = String::new(); // Create the variable to store the first param
+    io::stdin().read_line(&mut first_param).expect("Failed to read the param."); // Get the first param
+    let first_param: u32 = first_param.trim().parse().expect("Please enter a number!"); // Convert the first_param to a number
+    println!("{}", "Enter the second random number param: ".blue()); // Ask the user to input the second param
+    let mut second_param = String::new(); // Create the variable to store the second param
+    io::stdin().read_line(&mut second_param).expect("Failed to read the param."); // Get the second param
+    let second_param: u32 = second_param.trim().parse().expect("Please enter a number!"); // Convert the second param to a number
+
+    let secret_number = rand::thread_rng().gen_range(first_param, second_param); // Generate the random number
 
     loop {
 
@@ -23,7 +31,7 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("{}\n", "Too small!".red()), // Alert if the user guess is to small
             Ordering::Greater => println!("{}\n", "Too big!".red()), // Alert if the user guess is too big
-            Ordering::Equal => { // Alert if the user guess is equal to secret num and break the loop
+            Ordering::Equal => { // Alert if the user guess is equal to a secret num and break the loop
                 println!("{}\n", "You win!".green());
                 break;
             }
